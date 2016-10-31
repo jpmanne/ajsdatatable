eb836ab0b4084690ebfe3d596cb874dfb6c1d2cf
Jaya Prakash Manne <jayaprakash.manne@sciits.com>
2016-10-31 22:37:22 +0530

    Updated with calling an external public webservice

--- js/ajsdtcontroller.js
+++ js/ajsdtcontroller.js
@@ -1,15 +1,13 @@
 
 //====================================================================================
 
-ajsDTModule.controller('ajsDTController', function ($scope) {
-	var employees = [
-                  	{name  : 'Jaya Prakash', lastName  : 'Manne', gender  : 'Male', dateOfBirth: new Date("July 15, 1980"), salary : 55000.58},
-                  	{name  : 'Radha Devi', lastName  : 'Manne', gender  : 'Female', dateOfBirth: new Date("February 03, 1983"), salary : 30000},
-                  	{name  : 'Cherish Keshto', lastName  : 'Manne', gender  : 'Male', dateOfBirth: new Date("April 29, 2009"), salary : 5000}, 
-                  	{name  : 'Udvitha Keshto', lastName  : 'Manne', gender  : 'Female', dateOfBirth: new Date("August 11, 2012"), salary : 3000} 
-                 ];
-	$scope.employees = employees;
-	$scope.sortColumn = 'name';
+ajsDTModule.controller('ajsDTController', function ($scope, $http) {
+	$http.get('https://jsonplaceholder.typicode.com/users')
+		.then(function (response) {
+			$scope.employees = response.data;
+		});
+	
+	$scope.sortColumn = 'id';
 	$scope.reverseSort = false;
 	
 	$scope.sortData = function (column) {
